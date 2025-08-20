e1ab81da892e461cd9315e1637a83f54a1cf6a64
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2025-08-20 19:55:03 -0300

    Crear proyecto
    El proyecto está configurado tal que compile bien y se conecte a una
    base de datos PostgreSQL correctamente.

---
+++ backend/src/main/java/ar/edu/uncuyo/gimnasio_sport/GimnasioSportApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uncuyo.gimnasio_sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasioSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasioSportApplication.class, args);
+	}
+
+}
