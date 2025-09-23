8f766952ee45b18a4ebfe36240327558f3f675aa
LuciiAl <alvarez.lucia@uncuyo.edu.ar>
2025-09-23 01:56:48 -0300

    form alta empleado

--- src/main/java/ar/edu/uncuyo/gimnasio_sport/service/EmpleadoService.java
+++ src/main/java/ar/edu/uncuyo/gimnasio_sport/service/EmpleadoService.java
@@ -1,20 +1,27 @@
 package ar.edu.uncuyo.gimnasio_sport.service;
 
 import ar.edu.uncuyo.gimnasio_sport.dto.EmpleadoCreateForm;
+import ar.edu.uncuyo.gimnasio_sport.dto.EmpleadoResumenDto;
+import ar.edu.uncuyo.gimnasio_sport.dto.SocioResumenDto;
 import ar.edu.uncuyo.gimnasio_sport.entity.Empleado;
+import ar.edu.uncuyo.gimnasio_sport.entity.Socio;
 import ar.edu.uncuyo.gimnasio_sport.enums.RolUsuario;
 import ar.edu.uncuyo.gimnasio_sport.enums.TipoEmpleado;
 import ar.edu.uncuyo.gimnasio_sport.error.BusinessException;
+import ar.edu.uncuyo.gimnasio_sport.mapper.EmpleadoMapper;
 import ar.edu.uncuyo.gimnasio_sport.repository.EmpleadoRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EmpleadoService {
     private final EmpleadoRepository empleadoRepository;
     private final PersonaService personaService;
+    private final EmpleadoMapper empleadoMapper;
 
     @Transactional
     public Empleado crearEmpleado(EmpleadoCreateForm empleadoCreateForm) {
@@ -28,11 +35,24 @@ public class EmpleadoService {
         return empleadoRepository.save(empleado);
     }
 
+    public Empleado eliminarEmpleado(Long id) {
+        Empleado empleado = empleadoRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("Empleado no encontrado con id: " + id));
+        empleado.setEliminado(true);
+        return empleadoRepository.save(empleado);
+    }
+
     private RolUsuario determineRoleFromEmployeeType(TipoEmpleado tipoEmpleado) {
         return switch (tipoEmpleado) {
             case PROFESOR -> RolUsuario.PROFESOR;
             case ADMINISTRATIVO -> RolUsuario.ADMINISTRATIVO;
             default -> throw new BusinessException("Rol no definido para este tipo de empleado");
         };
+    }
+
+    @Transactional
+    public List<EmpleadoResumenDto> listarEmpleadoResumenDtos() {
+        List<Empleado> empleados = empleadoRepository.findAllByEliminadoFalse();
+        return empleadoMapper.toResumenDtos(empleados);
     }
 }
